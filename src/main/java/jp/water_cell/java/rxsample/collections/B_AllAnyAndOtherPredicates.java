ea9b51bb469ab3b7dae879a1c705e9b3cd6902ed
Yukiya Nakagawa <y.nakagawa@water-cell.jp>
2015-05-01 17:43:20 +0900

    Add answer for B_AllAnyAndOtherPredicates

--- src/main/java/jp/water_cell/java/rxsample/collections/B_AllAnyAndOtherPredicates.java
+++ src/main/java/jp/water_cell/java/rxsample/collections/B_AllAnyAndOtherPredicates.java
@@ -23,31 +23,39 @@ public class B_AllAnyAndOtherPredicates implements ICollectionUtils {
 
     public Boolean isFrom(Customer customer, City city) {
         // Return true if the customer is from the given city
-        // TODO
-        return null;
+        return customer.getCity().equals(city);
     }
 
     public Boolean checkAllCustomersAreFrom(Shop shop, City city) {
         // Return true if all customers are from the given city
-        // TODO
-        return null;
+        return Observable.from(shop.getCustomers())
+                .all(customer -> isFrom(customer, city))
+                .toBlocking()
+                .single();
     }
 
     public Boolean hasCustomerFrom(Shop shop, City city) {
         // Check there is at least one customer from the given city
-        // TODO
-        return null;
+        return Observable.from(shop.getCustomers())
+                .exists(customer -> isFrom(customer, city))
+                .toBlocking()
+                .single();
     }
 
     public Integer countCustomersFrom(Shop shop, City city) {
         // Returns the number of customers from the given city
-        // TODO
-        return null;
+        return Observable.from(shop.getCustomers())
+                .filter(customer -> isFrom(customer, city))
+                .count()
+                .toBlocking()
+                .single();
     }
 
     public Customer findAnyCustomerFrom(Shop shop, City city) {
         // Return a customer who lives in the given city or null if there is none
-        // TODO
-        return null;
+        return Observable.from(shop.getCustomers())
+                .first(customer -> isFrom(customer, city))
+                .toBlocking()
+                .single();
     }
 }
