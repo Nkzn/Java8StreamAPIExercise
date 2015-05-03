b3b97621ec14e153f19b193fe0ec3a11362613ed
Yukiya Nakagawa <y.nakagawa@water-cell.jp>
2015-05-03 15:38:08 +0900

    Add MinMax question

---
+++ src/main/java/jp/water_cell/java/rxsample/collections/D_MaxMin.java
@@ -0,0 +1,33 @@
+package jp.water_cell.java.rxsample.collections;
+
+import java.util.List;
+
+import jp.water_cell.java.rxsample.collections.models.Customer;
+import jp.water_cell.java.rxsample.collections.models.Product;
+import jp.water_cell.java.rxsample.collections.models.Shop;
+import rx.Observable;
+
+import static rx.math.operators.OperatorMinMax.*;
+
+public class D_MaxMin implements ICollectionUtils {
+
+    void example() {
+        Integer max = max(Observable.just(1, 42, 4))
+                .toBlocking()
+                .single();
+
+        List<String> longestString = maxBy(Observable.just("aaa", "bbbbb", "cc", "ddddd"), string -> string.length())
+                .toBlocking()
+                .single();
+    }
+
+    public Customer getCustomerWithMaximumNumberOfOrders(Shop shop) {
+        // Return a customer who ordered maximum number of orders
+        return null;
+    }
+
+    public Product getMostExpensiveOrderedProduct(Customer customer) {
+        // Return the most expensive ordered product
+        return null;
+    }
+}
---
+++ src/test/java/jp/water_cell/java/rxsample/D_Max_Min.java
@@ -0,0 +1,26 @@
+package jp.water_cell.java.rxsample;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import jp.water_cell.java.rxsample.collections.D_MaxMin;
+
+public class D_Max_Min implements ITestBase {
+
+    D_MaxMin sut;
+
+    public void setUp() {
+        sut = new D_MaxMin();
+    }
+
+    @Test
+    public void testCustomerWithMaximumNumberOfOrders() {
+        Assert.assertEquals(customers().get(reka), sut.getCustomerWithMaximumNumberOfOrders(shop()));
+    }
+
+    @Test
+    public void testTheMostExpensiveOrderedProduct() {
+        Assert.assertEquals(rubyMine, sut.getMostExpensiveOrderedProduct(customers().get(nathan)));
+    }
+
+}
