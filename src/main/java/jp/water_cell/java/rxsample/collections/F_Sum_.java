eacf52ad297de02980d18a2fbe3e71efa34ae4dd
Yukiya Nakagawa <y.nakagawa@water-cell.jp>
2015-05-05 13:28:45 +0900

    Add Sum answer

--- src/main/java/jp/water_cell/java/rxsample/collections/F_Sum_.java
+++ src/main/java/jp/water_cell/java/rxsample/collections/F_Sum_.java
@@ -3,7 +3,8 @@ package jp.water_cell.java.rxsample.collections;
 import jp.water_cell.java.rxsample.collections.models.Customer;
 import rx.Observable;
 
-import static rx.math.operators.OperatorSum.*;
+import static rx.math.operators.OperatorSum.sumDoubles;
+import static rx.math.operators.OperatorSum.sumIntegers;
 
 public class F_Sum_ {
 
@@ -16,7 +17,10 @@ public class F_Sum_ {
     public Double getTotalOrderPrice(Customer customer) {
         // Returns the sum of prices of all products ordered by customer.
         // Note: a customer may order the same product for several times.
-        // TODO
-        return null;
+        return sumDoubles(Observable.from(customer.getOrders())
+                .flatMap(order -> Observable.from(order.getProducts()))
+                .map(product -> product.getPrice()))
+                .toBlocking()
+                .single();
     }
 }
